2bc20aabdeff9a7b7548d812218d2612e4d93831
Pascal Strauch <pstrauch@ethz.ch>
2021-11-18 10:06:52 +0100

    Add ActionCmd pub, multy pages support, buttons

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,14 +6,68 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // create participant
+    dp = new DefaultParticipant(0, "selva");
+    // create publisher
+    action_cmd_pub = new DDSPublisher(idl_msg::QuadActionCmdPubSubType(),
+        "action_cmd", dp->participant());
+    // initialize publisher
+    action_cmd_pub->init();
 }
 
 MainWindow::~MainWindow()
 {
+    // delete all dynamically allocated instances
+    delete action_cmd_pub;
+    delete dp;
     delete ui;
 }
-void MainWindow::on_pushButton_4_released()
+
+void MainWindow::on_pB_takeoff_released()
 {
-    //TODO
+    action_cmd.cmd(3);
+    action_cmd_pub->publish(action_cmd);    //if pub->init() needed?
+    std::cout << "Takeoff cmd sent. \n";   //DEBUG
+    // proposal for action_cmd:
+    // 0: disarm
+    // 1: arm
+    // 2: reboot
+    // 3: takeoff
+    // 4: land
+}
+void MainWindow::on_pB_flightControls_released()
+{
+    ui->stackedWidget->setCurrentIndex(0);
+}
+
+
+void MainWindow::on_pB_status_released()
+{
+    ui->stackedWidget->setCurrentIndex(1);
+}
+
+
+void MainWindow::on_pB_arm_released()
+{
+    action_cmd.cmd(1);
+    action_cmd_pub->publish(action_cmd);
+    std::cout << "Arm cmd sent. \n";
+}
+
+
+void MainWindow::on_pB_disarm_released()
+{
+    action_cmd.cmd(0);
+    action_cmd_pub->publish(action_cmd);
+    std::cout << "Disarm cmd sent. \n";
+}
+
+
+void MainWindow::on_pB_land_released()
+{
+    action_cmd.cmd(4);
+    action_cmd_pub->publish(action_cmd);
+    std::cout << "Land cmd sent. \n";
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,10 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "QuadActionCmdPubSubTypes.h"
+#include "default_participant.h"
+#include "default_publisher.h"
+#include "quadcopter_msgs/msgs/QuadActionCmd.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -16,9 +20,24 @@ public:
     ~MainWindow();
 
 private slots:
-    void on_pushButton_4_released();
+    void on_pB_takeoff_released();
+
+    void on_pB_flightControls_released();
+
+    void on_pB_status_released();
+
+    void on_pB_arm_released();
+
+    void on_pB_disarm_released();
+
+    void on_pB_land_released();
 
 private:
     Ui::MainWindow *ui;
+
+    DefaultParticipant* dp;
+    DDSPublisher* action_cmd_pub;
+    idl_msg::QuadActionCmd action_cmd;
+
 };
 #endif // MAINWINDOW_H
